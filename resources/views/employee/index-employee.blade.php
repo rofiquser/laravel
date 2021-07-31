41b3d661a07f6fb13535ba864a4e83b99b627274
webmvw <masudrana.bbpi@gmail.com>
2021-07-31 10:23:02 +0600

    employee view template design

---
+++ resources/views/employee/index-employee.blade.php
@@ -0,0 +1,143 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    
+    <div class="container">
+    <div class="row">
+          <div class="col-lg-12 col-md-12 col-sm-12">
+              <br>
+              <br>
+            <div class="card">
+              <div class="card-header d-flex justify-content-between align-items-center">
+                <h3 class="card-title">All Employee List</h3>
+                <a href="#" class="btn btn-success btn-sm"><i class="fa fa-plus-circle"></i> Add Employee</a>
+              </div>
+              <!-- /.card-header -->
+                <div class="card-body">
+                <table class="table table-striped table-bordered">
+                <thead class="table-dark">
+                    <tr>
+                        <th>SL</th>
+                        <th>Name</th>
+                        <th>email</th>
+                        <th>department</th>
+                        <th>designation</th>
+                        <th>salary</th>
+                        <th>Address</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>01</td>
+                        <td>Faisal Habib</td>
+                        <td>faisal234@gmail.com</td>
+                        <td>Wordpress</td>
+                        <td>web developer</td>
+                        <td>20000</td>
+                        <td>Dhaka</td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-success">Edit</a>
+                            <a href="#" class="btn btn-sm btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>01</td>
+                        <td>Faisal Habib</td>
+                        <td>faisal234@gmail.com</td>
+                        <td>Wordpress</td>
+                        <td>web developer</td>
+                        <td>20000</td>
+                        <td>Dhaka</td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-success">Edit</a>
+                            <a href="#" class="btn btn-sm btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>01</td>
+                        <td>Faisal Habib</td>
+                        <td>faisal234@gmail.com</td>
+                        <td>Wordpress</td>
+                        <td>web developer</td>
+                        <td>20000</td>
+                        <td>Dhaka</td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-success">Edit</a>
+                            <a href="#" class="btn btn-sm btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>01</td>
+                        <td>Faisal Habib</td>
+                        <td>faisal234@gmail.com</td>
+                        <td>Wordpress</td>
+                        <td>web developer</td>
+                        <td>20000</td>
+                        <td>Dhaka</td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-success">Edit</a>
+                            <a href="#" class="btn btn-sm btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>01</td>
+                        <td>Faisal Habib</td>
+                        <td>faisal234@gmail.com</td>
+                        <td>Wordpress</td>
+                        <td>web developer</td>
+                        <td>20000</td>
+                        <td>Dhaka</td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-success">Edit</a>
+                            <a href="#" class="btn btn-sm btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>01</td>
+                        <td>Faisal Habib</td>
+                        <td>faisal234@gmail.com</td>
+                        <td>Wordpress</td>
+                        <td>web developer</td>
+                        <td>20000</td>
+                        <td>Dhaka</td>
+                        <td>
+                            <a href="#" class="btn btn-sm btn-success">Edit</a>
+                            <a href="#" class="btn btn-sm btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+                </div>
+                <!-- /.card-body -->
+                <div class="card-footer"></div>
+            </div>
+          </div>
+
+        </div><!-- /.row (main row) -->
+    </div>
+
+    
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
